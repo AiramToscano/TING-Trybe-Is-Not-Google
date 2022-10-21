f1a7a2daf1232b78287fb0f6bacd532ac9dcbb2f
Airam Toscano <airamlobato@gmail.com>
2022-10-21 10:43:22 -0400

    Feat: file_process.py

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+# from ting_file_management.queue import Queue
+from .file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    fila = instance._data
+    if (path_file not in fila):
+        instance.enqueue(path_file)
+    fileProcess = txt_importer(path_file)
+    obj = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(fileProcess),
+        "linhas_do_arquivo": fileProcess
+
+    }
+    sys.stdout.write(str(obj))
 
 
 def remove(instance):
@@ -8,3 +23,6 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+# print(process('ting_file_management/teste.txt', Queue()))
