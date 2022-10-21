ddae9fdb0c4d32fd55b47d1f4c2f2ce6a8016879
Airam Toscano <airamlobato@gmail.com>
2022-10-20 21:13:26 -0400

    Feat: ting_file_management/file_management.py

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    caminhoCerto = path_file.split('.')
+    try:
+        if (caminhoCerto[1] == 'txt'):
+            with open(path_file) as f:
+                # https://www.pythontutorial.net/python-basics/python-read-text-file/
+                lines = [line.strip() for line in f.readlines()]
+            return lines
+        else:
+            # https://stackoverflow.com/questions/5574702/how-do-i-print-to-stderr-in-python
+            sys.stderr.write("Formato inválido\n")
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+
+# print(txt_importer('ting_file_management/teste.txt'))
